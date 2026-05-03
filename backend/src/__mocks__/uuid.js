cbff889be294977ba2fffe0670138a0788642401
BENALS <skander.bensalah@esprit.tn>
2026-05-03 19:52:04 +0100

    securite

--- backend/src/__mocks__/uuid.js
+++ backend/src/__mocks__/uuid.js
@@ -6,7 +6,7 @@ const crypto = require('crypto');
 
 function v4() {
   return crypto.randomUUID ? crypto.randomUUID() : 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-    const r = (Math.random() * 16) | 0;
+    const r = crypto.randomBytes(1)[0] % 16;
     const v = c === 'x' ? r : (r & 0x3) | 0x8;
     return v.toString(16);
   });
